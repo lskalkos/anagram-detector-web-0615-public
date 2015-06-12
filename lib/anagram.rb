908a6057bea6c7b0354472d9fc1dffa44cd03a72
Lia Skalkos <hello@liaskalkos.net>
2015-06-11 21:44:42 -0400

    Create anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,40 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+
+    word_letters = @word.split("")
+    matches = words.collect do |string|
+      match_letters = string.split("")
+      match = true
+
+      if match_letters.length == word_letters.length && match_letters.uniq.length == word_letters.uniq.length 
+        match_letters.each do |letter|
+          unless word_letters.include?(letter)
+            match = false 
+          end
+        end
+      else
+        match = false
+      end
+
+      string if match
+
+    end
+
+    matches.compact
+
+  end#end method
+
+end
+
+
+
+
+
